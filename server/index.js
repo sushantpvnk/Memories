cd0d9074330251fa1568eb790b7ba60c1a290c14
Vighnesh2304 <vkandalgavkar850@gmail.com>
2024-08-17 14:02:10 +0530

    error resolved

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = process.env.CONNECTION_URL;
+const CONNECTION_URL = 'mongodb+srv://vkandalgavkar850:vighnesh2304@cluster0.cfohy30.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
